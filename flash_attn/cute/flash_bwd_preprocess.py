demonatic/flash-attention

made the flash_attn/cute a new package named flash_attn_cute, with its own setup.py and requirements.txt

flash_attn/cute/flash_bwd_preprocess.py
@@ -11,10 +11,10 @@ import cutlass
 import cutlass.cute as cute
 from cutlass import Float32
 
-from flash_attn.cute import utils
-from flash_attn.cute import copy_utils
-from flash_attn.cute.seqlen_info import SeqlenInfoQK
-from flash_attn.cute.tile_scheduler import (
+from flash_attn_cute import utils
+from flash_attn_cute import copy_utils
+from flash_attn_cute.seqlen_info import SeqlenInfoQK
+from flash_attn_cute.tile_scheduler import (
     ParamsBase,
     SingleTileScheduler,
     SingleTileVarlenScheduler,

littsk/flexible-flash-attention

flash_attn/cute/flash_bwd_preprocess.py
@@ -25,10 +25,10 @@ from cutlass.cutlass_dsl import Arch, BaseDSL
 
 from quack import copy_utils, layout_utils
 
-from flash_attn.cute import utils
-from flash_attn.cute.seqlen_info import SeqlenInfo
+from flash_attn_cute import utils
+from flash_attn_cute.seqlen_info import SeqlenInfo
 from quack.cute_dsl_utils import ParamsBase
-from flash_attn.cute.tile_scheduler import (
+from flash_attn_cute.tile_scheduler import (
     SingleTileScheduler,
     SingleTileVarlenScheduler,
     TileSchedulerArguments,